c7d4253057e1ab12b84ad024489f16d8d0c2de04
= <=>
2024-03-04 20:46:37 +0100

    Creación de la conexión con mysql

---
+++ conectar.py
@@ -0,0 +1,21 @@
+import MySQLdb
+
+db = MySQLdb.connect(host="localhost",    # tu host, usualmente localhost
+                     user="root",         # tu usuario
+                     passwd="root",  # tu password
+                     db="empresas")        # el nombre de la base de datos
+
+# Debes crear un objeto Cursor. Te permitirá
+# ejecutar todos los queries que necesitas
+cur = db.cursor()
+
+# Usa todas las sentencias SQL que quieras
+cur.execute("SELECT * FROM GE_EMPRESAS")
+
+# Imprimir la primera columna de todos los registros
+for row in cur.fetchall():
+    print(row[2])
+
+# no hay que olvidarse de liberar el cursor y la conexión para evitar fugas de memoria
+cur.close()
+db.close()
